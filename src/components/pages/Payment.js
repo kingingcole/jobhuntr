d5c68583f94c0f0dd428e45845b295acf3b77db7
kigningcole <43896019+kigningcole@users.noreply.github.com>
2019-05-30 00:27:16 +0200

    Added stripe functionalities but havent bounded to job addiion

--- src/components/pages/Payment.js
+++ src/components/pages/Payment.js
@@ -3,18 +3,20 @@ import {Elements, StripeProvider} from 'react-stripe-elements';
 import CheckoutForm from '../atoms/CheckoutForm';
 
 class Payment extends Component {
-  render() {
-    return (
-      <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
-        <div className="example">
-          <h1>React Stripe Elements Example</h1>
-          <Elements>
-            <CheckoutForm />
-          </Elements>
-        </div>
-      </StripeProvider>
-    );
-  }
+    render() {
+        return (
+            <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+                <section>
+                    <div className="container">
+                        <h1>React Stripe Elements Example</h1>
+                        <Elements>
+                            <CheckoutForm/>
+                        </Elements>
+                    </div>
+                </section>
+            </StripeProvider>
+        );
+    }
 }
 
 export default Payment;
