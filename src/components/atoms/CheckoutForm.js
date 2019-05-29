de2df0274a998c766bafecb4192089034bebfc87
kingingcole <43896019+kingingcole@users.noreply.github.com>
2019-05-29 13:46:47 +0100

    Update CheckoutForm.js

--- src/components/atoms/CheckoutForm.js
+++ src/components/atoms/CheckoutForm.js
@@ -4,6 +4,7 @@ import {CardElement, injectStripe} from 'react-stripe-elements';
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {complete: false};
     this.submit = this.submit.bind(this);
   }
 
@@ -16,10 +17,12 @@ class CheckoutForm extends Component {
       body: token.id
     });
 
-    if (response.ok) console.log("Purchase Complete!")
+    if (response.ok) this.setState({complete: true});
   }
 
   render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>;
+
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
