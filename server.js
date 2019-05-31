785b8a035e9631473ef3ad1201140ff3babfcb00
kigningcole <43896019+kigningcole@users.noreply.github.com>
2019-06-01 00:55:57 +0200

    further work on stripe done

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const app = express();
-const stripe = require("stripe")("sk_test_aIx7sOwerYh2IIBhEkVxL9rm00w97FMzEI");
-;
+
+
 
 app.use(express.json());
 
 app.post("/charge", async (req, res) => {
     try {
-        let {amount, tokenId} = req.body;
-        console.log(amount, tokenId);
-        console.log(x);
+        let {amount, tokenId, stripe_key} = req.body; // params sent in with client
+        const stripe = require("stripe")(stripe_key); // initializes stripe with the stripe keys passed from client
+        // console.log(amount, tokenId, stripe_key);
         let {status} = await stripe.charges.create({
             amount,
             currency: "usd",
             description: "An example charge",
-            source: 'tok_chargeDeclinedInsufficientFunds'
+            source: tokenId
         });
-
+        console.log(status);
         res.json({status});
     } catch (err) {
-        res.status(500).end();
-        res.status(err).end();
+        // res.status(500).end();
+        res.json({err}).end();
         console.log(err)
     }
 });
 
-app.listen(9000, () => console.log("Listening on port 9000", x));
+app.listen(9000, () => console.log("Listening on port 9000"));
--- src/components/atoms/CheckoutForm.js
+++ src/components/atoms/CheckoutForm.js
@@ -7,9 +7,11 @@ let stripe_sk_test = process.env.REACT_APP_STRIPE_SECRET_TEST_KEY;
 let stripe_sk_live = process.env.REACT_APP_STRIPE_SECRET_LIVE_KEY;
 let node_env = process.env.NODE_ENV;
 
-let stripe_key = node_env === 'development' ? stripe_sk_test : stripe_sk_live
+// checks node envirpnment whether dev or production
+// if dev use the test stripe keys
+// if production use the stripe live keys
+let stripe_key = node_env === 'development' ? stripe_sk_test : stripe_sk_live;
 
-console.log(stripe_key);
 
 class CheckoutForm extends Component {
     constructor(props) {
@@ -21,7 +23,8 @@ class CheckoutForm extends Component {
     async submit(ev) {
         // User clicked submit
         let {token} = await this.props.stripe.createToken({name: "Name"});
-        console.log(token.id)
+        console.log(token.id);
+        let tokenId = node_env === 'development' ?  'tok_visa' : token.id;
         let response = await fetch("/charge", {
             method: "POST",
             headers: {
@@ -30,7 +33,8 @@ class CheckoutForm extends Component {
             },
             body: JSON.stringify({
                 amount: 2000,
-                tokenId: token.id
+                tokenId,
+                stripe_key
             })
         });
 
@@ -42,7 +46,6 @@ class CheckoutForm extends Component {
     }
 
     render() {
-
         if (this.state.complete) return <h1>Purchase Complete</h1>;
 
         return (
